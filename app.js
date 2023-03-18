190debc91cf6b4813c8a65a77d7974e580ddb630
thzero <thzero@users.noreply.github.com>
2023-03-18 16:09:03 -0500

    updates

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import AppConstants from '@/utility/constants';
+import AppCommonConstants from 'rocket_sidekick_common/constants';
 
 import SettingsUser from 'rocket_sidekick_common/data/settingsUser';
 
@@ -29,83 +29,83 @@ class AppUtility {
 	static initializeSettingsUser() {
 		const settings = new SettingsUser();
 		settings.measurementUnits = {
-			id: AppConstants.MeasurementUnits.english.id
+			id: AppCommonConstants.MeasurementUnits.english.id
 		};
-		settings.measurementUnits.id = AppConstants.MeasurementUnits.english.id;
-		settings.measurementUnits.acceleration = AppConstants.MeasurementUnits.english.acceleration.default;
-		settings.measurementUnits.area = AppConstants.MeasurementUnits.english.area.default;
-		settings.measurementUnits.distance = AppConstants.MeasurementUnits.english.distance.default;
-		settings.measurementUnits.length = AppConstants.MeasurementUnits.english.length.default;
-		settings.measurementUnits.velocity = AppConstants.MeasurementUnits.english.velocity.default;
-		settings.measurementUnits.volume = AppConstants.MeasurementUnits.english.volume.default;
-		settings.measurementUnits.weight = AppConstants.MeasurementUnits.english.weight.default;
+		settings.measurementUnits.id = AppCommonConstants.MeasurementUnits.english.id;
+		settings.measurementUnits.acceleration = AppCommonConstants.MeasurementUnits.english.acceleration.default;
+		settings.measurementUnits.area = AppCommonConstants.MeasurementUnits.english.area.default;
+		settings.measurementUnits.distance = AppCommonConstants.MeasurementUnits.english.distance.default;
+		settings.measurementUnits.length = AppCommonConstants.MeasurementUnits.english.length.default;
+		settings.measurementUnits.velocity = AppCommonConstants.MeasurementUnits.english.velocity.default;
+		settings.measurementUnits.volume = AppCommonConstants.MeasurementUnits.english.volume.default;
+		settings.measurementUnits.weight = AppCommonConstants.MeasurementUnits.english.weight.default;
 		return settings;
 	}
 
 	static measurementUnitsId(correlationId, settings) {
-		return settings && settings.measurementUnits && settings.measurementUnits.id ? settings.measurementUnits.id : AppConstants.MeasurementUnits.english.id;
+		return settings && settings.measurementUnits && settings.measurementUnits.id ? settings.measurementUnits.id : AppCommonConstants.MeasurementUnits.english.id;
 	}
 
 	static measurementUnits(correlationId, settings) {
-		return AppConstants.MeasurementUnits[AppUtility.measurementUnitsId(correlationId, settings)];
+		return AppCommonConstants.MeasurementUnits[AppUtility.measurementUnitsId(correlationId, settings)];
 	}
 
 	static measurementUnitsAccelerationId(correlationId, settings, measurementUnitsId) {
 		measurementUnitsId = !String.isNullOrEmpty(measurementUnitsId) ? measurementUnitsId : AppUtility.measurementUnitsId(correlationId, settings);
 		return settings && settings.measurementUnits && settings.measurementUnits.acceleration ? settings.measurementUnits.acceleration
-			: AppConstants.MeasurementUnits[measurementUnitsId].acceleration.default;
+			: AppCommonConstants.MeasurementUnits[measurementUnitsId].acceleration.default;
 	}
 
 	static measurementUnitAreaId(correlationId, settings, measurementUnitsId) {
 		measurementUnitsId = !String.isNullOrEmpty(measurementUnitsId) ? measurementUnitsId : AppUtility.measurementUnitsId(correlationId, settings);
 		return settings && settings.measurementUnits && settings.measurementUnits.area ? settings.measurementUnits.area
-			: AppConstants.MeasurementUnits[measurementUnitsId].area.default;
+			: AppCommonConstants.MeasurementUnits[measurementUnitsId].area.default;
 	}
 
 	static measurementUnitDensityId(correlationId, settings, measurementUnitsId) {
 		measurementUnitsId = !String.isNullOrEmpty(measurementUnitsId) ? measurementUnitsId : AppUtility.measurementUnitsId(correlationId, settings);
 		return settings && settings.measurementUnits && settings.measurementUnits.density ? settings.measurementUnits.density
-			: AppConstants.MeasurementUnits[measurementUnitsId].density.default;
+			: AppCommonConstants.MeasurementUnits[measurementUnitsId].density.default;
 	}
 
 	static measurementUnitDistanceId(correlationId, settings, measurementUnitsId) {
 		measurementUnitsId = !String.isNullOrEmpty(measurementUnitsId) ? measurementUnitsId : AppUtility.measurementUnitsId(correlationId, settings);
 		return settings && settings.measurementUnits && settings.measurementUnits.distance ? settings.measurementUnits.distance
-			: AppConstants.MeasurementUnits[measurementUnitsId].distance.default;
+			: AppCommonConstants.MeasurementUnits[measurementUnitsId].distance.default;
 	}
 
 	static measurementUnitFluidId(correlationId, settings, measurementUnitsId) {
 		measurementUnitsId = !String.isNullOrEmpty(measurementUnitsId) ? measurementUnitsId : AppUtility.measurementUnitsId(correlationId, settings);
 		return settings && settings.measurementUnits && settings.measurementUnits.fluid ? settings.measurementUnits.fluid
-			: AppConstants.MeasurementUnits[measurementUnitsId].fluid.default;
+			: AppCommonConstants.MeasurementUnits[measurementUnitsId].fluid.default;
 	}
 
 	static measurementUnitLengthId(correlationId, settings, measurementUnitsId) {
 		measurementUnitsId = !String.isNullOrEmpty(measurementUnitsId) ? measurementUnitsId : AppUtility.measurementUnitsId(correlationId, settings);
 		return settings && settings.measurementUnits && settings.measurementUnits.length ? settings.measurementUnits.length
-			: AppConstants.MeasurementUnits[measurementUnitsId].length.default;
+			: AppCommonConstants.MeasurementUnits[measurementUnitsId].length.default;
 	}
 
 	static measurementUnitVelocityId(correlationId, settings, measurementUnitsId) {
 		measurementUnitsId = !String.isNullOrEmpty(measurementUnitsId) ? measurementUnitsId : AppUtility.measurementUnitsId(correlationId, settings);
 		return settings && settings.measurementUnits && settings.measurementUnits.velocity ? settings.measurementUnits.velocity
-			: AppConstants.MeasurementUnits[measurementUnitsId].velocity.default;
+			: AppCommonConstants.MeasurementUnits[measurementUnitsId].velocity.default;
 	}
 
 	static measurementUnitVolumeId(correlationId, settings, measurementUnitsId) {
 		measurementUnitsId = !String.isNullOrEmpty(measurementUnitsId) ? measurementUnitsId : AppUtility.measurementUnitsId(correlationId, settings);
 		return settings && settings.measurementUnits && settings.measurementUnits.volume ? settings.measurementUnits.volume
-			: AppConstants.MeasurementUnits[measurementUnitsId].volume.default;
+			: AppCommonConstants.MeasurementUnits[measurementUnitsId].volume.default;
 	}
 
 	static measurementUnitWeightId(correlationId, settings, measurementUnitsId) {
 		measurementUnitsId = !String.isNullOrEmpty(measurementUnitsId) ? measurementUnitsId : AppUtility.measurementUnitsId(correlationId, settings);
 		return settings && settings.measurementUnits && settings.measurementUnits.weight ? settings.measurementUnits.weight
-			: AppConstants.MeasurementUnits[measurementUnitsId].weight.default;
+			: AppCommonConstants.MeasurementUnits[measurementUnitsId].weight.default;
 	}
 
 	static measurementUnitsOptions() {
-		return [ AppConstants.MeasurementUnits.english.id, AppConstants.MeasurementUnits.metrics.id ];
+		return [ AppCommonConstants.MeasurementUnits.english.id, AppCommonConstants.MeasurementUnits.metrics.id ];
 	}
 
 	static userDisplayName(correlationId, user) {
--- constants.js
+++ constants.js
@@ -16,10 +16,6 @@ const Constants = {
 	ExternalKeys: {
 		MotorSearch: 'motorSearch'
 	},
-	ExtractTypes: {
-		Csv: 'csv',
-		Text: 'text'
-	},
 	Features: {
 		Auth: false,
 		CookieComply: true,
@@ -38,165 +34,16 @@ const Constants = {
 			Rockets: false
 		}
 	},
-	FlightInfo: {
-		Resolution: 1024
-	},
 	InjectorKeys: {
 		SERVICE_API: 'serviceApi',
-		SERVICE_DATABASE: 'serviceDatabase',
-		SERVICE_DATABASE_MANUFACTURERS: 'serviceDatabaseManufacturers',
-		SERVICE_DOWNLOAD: 'serviceDownload',
 		SERVICE_EXTERNAL_MOTOR_SEARCH: 'serviceExternalMotorSearch',
 		SERVICE_MANUFACTURERS: 'serviceManufacturers',
-		SERVICE_REST_COMMUNICATION_SECONDARY: 'serviceRestCommunicationSecondary',
+		SERVICE_PARTS: 'serviceParts',
 		SERVICE_ROCKETS: 'serviceRockets',
 		SERVICE_TOOLS_CALCULATION_ENGINE: 'serviceToolsCalculationEngine',
-		SERVICE_TOOLS_FOAM: 'serviceToolsFoam',
-		SERVICE_TOOLS_FLIGHT_INFO_PROCESSOR: 'serviceToolsFlightInfoProcessor',
-		SERVICE_TOOLS_FLIGHT_INFO_PROCESSOR_EGGTIMER: 'serviceToolsFlightInfoProcessorEggtimer',
-		SERVICE_TOOLS_FLIGHT_PATH_OUTPUT: 'serviceToolsFlightPathOutput',
-		SERVICE_TOOLS_FLIGHT_PATH_OUTPUT_KML: 'serviceToolsFlightPathOutputKml',
-		SERVICE_TOOLS_FLIGHT_PATH_OUTPUT_TEMPLATE: 'serviceToolsFlightPathOutputTemplate',
-		SERVICE_TOOLS_FLIGHT_PATH_OUTPUT_TEMPLATE_HANDLEBARS: 'serviceToolsFlightPathOutputTemplateHandlebars',
-		SERVICE_TOOLS_FLIGHT_PATH_PROCESSOR: 'serviceToolsFlightPathProcessor',
-		SERVICE_TOOLS_FLIGHT_PATH_PROCESSOR_FEATHERWEIGHT: 'serviceToolsFlightPathProcessorFeatherweight',
 		SERVICE_TOOLS_PARACHUTE_SIZING: 'serviceToolsParachuteSizing',
 		SERVICE_TOOLS_THRUST2WEIGHT: 'serviceToolsThrust2Weight',
 		SERVICE_TOOLS_WEATHERCOCKING: 'serviceToolsWeathercocking'
-	},
-	MeasurementUnits: {
-		acceleration: {
-			id: 'acceleration'
-		},
-		area: {
-			id: 'area'
-		},
-		density: {
-			id: 'density'
-		},
-		distance: {
-			id: 'distance'
-		},
-		fluid: {
-			id: 'fluid'
-		},
-		length: {
-			id: 'length'
-		},
-		velocity: {
-			id: 'velocity'
-		},
-		volume: {
-			id: 'volume'
-		},
-		weight: {
-			id: 'weight'
-		},
-		english: {
-			id: 'english',
-			acceleration: {
-				default: 'fts2',
-				fts2: 'ft/s^2'
-			},
-			area: {
-				default: 'in2',
-				in2: 'in^2',
-				ft2: 'ft^2'
-			},
-			density: {
-				default: 'lbmft3',
-				lbmft3: 'lbm/ft3'
-			},
-			distance: {
-				default: 'ft',
-				ft: 'ft',
-				mi: 'mi'
-			},
-			fluid: {
-				default: 'floz',
-				floz: 'floz'
-			},
-			length: {
-				default: 'in',
-				in: 'in',
-				ft: 'ft'
-			},
-			velocity: {
-				default: 'fts',
-				fts: 'ft/s',
-				mph: 'mile/h'
-			},
-			volume: {
-				default: 'in3',
-				in3: 'in^3',
-				ft3: 'ft^3'
-			},
-			weight: {
-				default: 'oz',
-				oz: 'oz',
-				lb: 'lb'
-			}
-		},
-		metrics: {
-			id: 'metrics',
-			acceleration: {
-				default: 'ms2',
-				ms2: 'm/s^2'
-			},
-			area: {
-				default: 'mm2',
-				cm2: 'cm^2',
-				m2: 'm^2',
-				mm2: 'mm^2'
-			},
-			density: {
-				default: 'kgm3',
-				kgm3: 'kg/m^3'
-			},
-			distance: {
-				default: 'm',
-				m: 'm',
-				km: 'km'
-			},
-			fluid: {
-				default: 'ml',
-				ml: 'ml',
-				l: 'l'
-			},
-			length: {
-				default: 'mm',
-				mm: 'mm',
-				cm: 'cm',
-				m: 'm'
-			},
-			velocity: {
-				default: 'ms',
-				ms: 'm/s',
-				kph: 'km/h'
-			},
-			volume: {
-				default: 'mm3',
-				cm3: 'cm^3',
-				m3: 'm^3',
-				mm3: 'mm^3'
-			},
-			weight: {
-				default: 'g',
-				g: 'g',
-				kg: 'kg'
-			}
-		},
-		types: {
-			acceleration: 'acceleration',
-			area: 'area',
-			fluid: 'fluid',
-			density: 'density',
-			distance: 'distance',
-			length: 'length',
-			velocity: 'velocity',
-			volume: 'volume',
-			weight: 'weight'
-		}
 	},
 	RocketTypes: {
 		Site: 'site',
